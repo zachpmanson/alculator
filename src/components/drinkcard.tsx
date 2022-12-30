f01a1463d90e4bf66e93bba06b7216274d799e70
Zach Manson <zachpmanson@gmail.com>
2022-12-30 09:09:13 +0800

    Make badges pretty

---
+++ src/components/drinkcard.tsx
@@ -0,0 +1,52 @@
+import { Drink } from "../types";
+import Image from "next/image";
+import { useGlobal } from "../contexts/Global/context";
+
+type DrinkCardProps = {
+  item: Drink;
+};
+
+export default function DrinkCard({ item }: DrinkCardProps) {
+  const {
+    currentFilters: { pack },
+  } = useGlobal();
+
+  return (
+    <div className="card center">
+      <a href={`https://www.danmurphys.com.au/product/${item.stockcode}`}>
+        <div className="flex center-aligned">
+          <Image
+            alt="Image of drink"
+            height="100"
+            width="80"
+            src={`https://media.danmurphys.com.au/dmo/product/${item.stockcode}-1.png`}
+          />
+          <div className="fill-width">
+            <h3>{item.name}</h3>
+
+            <div className="flex badge-row">
+              <div className="badge">
+                <div className="badge-label">Price</div>
+                <div className="badge-number">${item.price}</div>
+              </div>
+              {(pack === "case" || pack === "pack") && (
+                <div className="badge">
+                  <div className="badge-label">Units</div>
+                  <div className="badge-number">{item.units[pack]}</div>
+                </div>
+              )}
+              <div className="badge">
+                <div className="badge-label">Standard Drinks</div>
+                <div className="badge-number">{Math.round(10 * item.strength) / 10}</div>
+              </div>
+              <div className="badge">
+                <div className="badge-label">SD/$</div>
+                <div className="badge-number">{Math.round(100 * item.ratio) / 100}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+}
--- src/components/drinkcardlist.tsx
+++ src/components/drinkcardlist.tsx
@@ -2,6 +2,7 @@ import { useGlobal } from "../contexts/Global/context";
 import Image from "next/image";
 import { useEffect, useState } from "react";
 import { Drink } from "../types";
+import DrinkCard from "./drinkcard";
 
 export default function DrinkCardList() {
   const {
@@ -11,30 +12,7 @@ export default function DrinkCardList() {
 
   const [lastDrinkIndex, setLastDrinkIndex] = useState(20);
 
-  const cards = currentDrinks.slice(0, lastDrinkIndex).map((item, index) => (
-    <div key={index} className="card center">
-      <a href={`https://www.danmurphys.com.au/product/${item.stockcode}`}>
-        <div className="flex center-aligned">
-          <Image
-            alt="Image of drink"
-            height="100"
-            width="80"
-            src={`https://media.danmurphys.com.au/dmo/product/${item.stockcode}-1.png`}
-          />
-          <div className="fill-width">
-            <h3>{item.name}</h3>
-
-            <div className="flex badge-row">
-              <div className="badge">${item.price}</div>
-              {(pack === "case" || pack === "pack") && <div className="badge">{item.units[pack]} units</div>}
-              <div className="badge">{Math.round(10 * item.strength) / 10} standard drinks</div>
-              <div className="badge">{Math.round(100 * item.ratio) / 100} SD/$</div>
-            </div>
-          </div>
-        </div>
-      </a>
-    </div>
-  ));
+  const cards = currentDrinks.slice(0, lastDrinkIndex).map((item, index) => <DrinkCard key={index} item={item} />);
 
   useEffect(() => {
     const onScroll = () => {
